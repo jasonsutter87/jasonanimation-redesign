0116b6979651cda58bee37cdcdb0fd67ee4e62b1
Jason Sutter <jasonsutter87@gmail.com>
2016-03-30 16:06:50 -0700

    Added users through devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :project_eulers
   resources :blogs
   get "/pages/:page" => "pages#show"
+  root to: "blogs#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
