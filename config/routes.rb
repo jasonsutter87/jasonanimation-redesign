86127226c5137e60680e87a6257d20e7af8d7fe4
Jason Sutter <jasonsutter87@gmail.com>
2016-03-30 15:34:17 -0700

    finishing up basic scadffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :project_eulers
   resources :blogs
-  #get "/pages/:page" => "pages#show"
+  get "/pages/:page" => "pages#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
